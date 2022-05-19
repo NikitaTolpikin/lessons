bd3b3c6c20cb8e256b3998b667e72a9c41e03614
NikitaTolpikin <georgin1106@gmail.com>
2022-05-19 21:19:26 +0300

    Add print thanks

--- programm.py
+++ programm.py
@@ -1,9 +1,8 @@
-def print_buy(name):
-    print(f'Buy {name}')
-
-
 def print_hi(name):
     print(f'Hi, {name}')
+
+def print_thanks(name):
+    print(f'Thanks {name}')
 
 
 if __name__ == '__main__':
